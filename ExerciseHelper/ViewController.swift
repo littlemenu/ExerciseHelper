236861a3e0d7150f0a99db9039b86831bc0c7555
littlemenu <jjhisking@gmail.com>
2019-09-04 17:04:25 +0900

    Initial Commit

---
+++ ExerciseHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExerciseHelper
+//
+//  Created by 정재훈 on 04/09/2019.
+//  Copyright © 2019 정재훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExerciseHelperTests/ExerciseHelperTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExerciseHelperTests.swift
+//  ExerciseHelperTests
+//
+//  Created by 정재훈 on 04/09/2019.
+//  Copyright © 2019 정재훈. All rights reserved.
+//
+
+import XCTest
+@testable import ExerciseHelper
+
+class ExerciseHelperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
